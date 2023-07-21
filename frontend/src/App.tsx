c89988826391ab3a48007c62bd3b3ef67f4079d3
mateussousaa <mateussousa20@outlook.com>
2023-07-21 17:20:50 -0300

    feat: creating the component FileUploader without submit action

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,20 @@
+import { styled } from 'styled-components'
+import FileUploader from './components/FileUploader'
+
+const Container = styled.div`
+  margin: auto;
+  max-width: 1200px;
+  padding: 8px;
+  min-height: 100vh;
+`
+
+const onFileUpload = () => {}
+
 function App() {
   return (
-    <>
-      <h1>fullstack afiliados</h1>
-    </>
+    <Container>
+      <FileUploader onFileUpload={onFileUpload} />
+    </Container>
   )
 }
 
---
+++ frontend/src/components/FileUploader.tsx
@@ -0,0 +1,127 @@
+// src/components/FileUploader.tsx
+import React, { useState, useRef } from 'react';
+import styled from 'styled-components';
+
+const FileUploaderWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+`;
+
+const StyledInputFile = styled.input`
+  display: none;
+`;
+
+const ChooseFileButton = styled.label`
+  padding: 10px 20px;
+  background-color: white;
+  color: black;
+  border: 1px solid black;
+  border-radius: 5px;
+  cursor: pointer;
+  white-space: nowrap;
+`;
+
+const UploadButton = styled.label`
+  padding: 10px 20px;
+  background-color: #4141a8;
+  color: white;
+  border-radius: 5px;
+  cursor: pointer;
+  white-space: nowrap;
+`;
+
+const SelectedFileName = styled.div`
+  font-size: 16px;
+  color: black;
+`;
+
+const DragDropArea = styled.div`
+  border: 2px dotted black;
+  border-radius: 5px;
+  padding: 25px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+  cursor: pointer;
+`;
+
+const UploadArea = styled.div`
+  display: flex;
+  gap: 16px;
+  align-items: center;
+`
+
+const UploadIcon = styled.img`
+  max-width: 80px;
+`;
+
+interface FileUploaderProps {
+  onFileUpload: (file: File) => void;
+}
+
+const FileUploader: React.FC<FileUploaderProps> = ({ onFileUpload }) => {
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    setSelectedFile(file || null);
+  };
+
+  const handleUpload = () => {
+    if (selectedFile) {
+      onFileUpload(selectedFile);
+    }
+  };
+
+  const handleDragOver = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+  };
+
+  const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    const file = event.dataTransfer.files?.[0];
+    setSelectedFile(file || null);
+  };
+
+  const openFileDialog = () => {
+    fileInputRef.current?.click();
+  };
+
+  return (
+    <FileUploaderWrapper>
+      <StyledInputFile
+        type="file"
+        id="fileInput"
+        ref={fileInputRef}
+        onChange={handleFileChange}
+      />
+      <DragDropArea
+        onClick={openFileDialog}
+        onDragOver={handleDragOver}
+        onDrop={handleDrop}
+      >
+        <UploadIcon src="/upload_icon.svg" alt="Upload Icon" />
+        {selectedFile ? (
+          <SelectedFileName>{selectedFile.name}</SelectedFileName>
+        ) : (
+          <>
+            <div style={{ textAlign: 'center'}}>Drag and drop your file here</div>
+            <span>or</span>
+            <ChooseFileButton>Choose File</ChooseFileButton>
+          </>
+        )}
+      </DragDropArea>
+      <UploadArea>
+        {selectedFile && <SelectedFileName>{selectedFile.name}</SelectedFileName>}
+        <UploadButton onClick={handleUpload}>Upload File</UploadButton>
+      </UploadArea>
+    </FileUploaderWrapper>
+  );
+};
+
+export default FileUploader;
