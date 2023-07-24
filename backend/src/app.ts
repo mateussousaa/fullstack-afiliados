60452a2aa1a129116601191b466ccf9e69cabd07
mateussousaa <mateussousa20@outlook.com>
2023-07-24 14:37:56 -0300

    feat: installing cors library

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,12 @@
 import express, { Application } from "express"
+import cors from 'cors'
 import { routes } from './routes'
 
 const app: Application = express()
+
+app.use(cors({
+  origin: 'localhost:3000'
+}))
 
 app.use(express.json())
 app.use(routes)
